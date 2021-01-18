c4c084c185a669ba73b47e543b2c1b216acbde90
ulrych8 <pavloffulysse@gmail.com>
2021-01-18 17:54:16 +0100

    cameraControl

---
+++ Assets/Script/FollowPlayer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class FollowPlayer : MonoBehaviour
+{
+
+	public Transform cameraTarget;
+	public Transform player;
+	public float cameraRotationSpeed = 2f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = cameraTarget.position;
+        transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, cameraRotationSpeed*Time.deltaTime);
+
+    }
+}
